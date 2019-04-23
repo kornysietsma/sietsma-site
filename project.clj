75b8f1a54e9ba929b04c5fcbe3f08bb6c7e45a63
Korny Sietsma <korny@sietsma.com>
2019-04-23 08:42:08 +0100

    attempt to update clojure versions - failed, going to go back to boring tech

--- project.clj
+++ project.clj
@@ -3,17 +3,17 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [stasis "2.2.2"]
-                 [ring "1.3.2"]
-                 [optimus "0.17.1"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [stasis "2.4.0"]
+                 [ring "1.7.1"]
+                 [optimus "0.20.2"]
                  [optimus-sass "0.0.3"]
                  [hiccup "1.0.5"]
-                 [enlive "1.1.5"]
-                 [clygments "0.1.1"]
+                 [enlive "1.1.6"]
+                 [clygments "2.0.0"]
                  [me.raynes/cegdown "0.1.1"]
                  [hiccup "1.0.5"]
                  [frontmatter "0.0.1"]]
   :ring {:handler sietsma-site.web/app}
   :aliases {"build-site" ["run" "-m" "sietsma-site.web/export"]}
-  :profiles {:dev {:plugins [[lein-ring "0.8.10"]]}})
+  :profiles {:dev {:plugins [[lein-ring "0.12.5"]]}})
